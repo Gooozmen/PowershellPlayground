e0231a206d31cdeddf07c9a263af995ce2ae6b45
Gooozmen <jguzman.softdev@gmail.com>
2024-12-18 21:30:03 -0300

    fix current location

--- build/createNugetPackage.ps1
+++ build/createNugetPackage.ps1
@@ -1,3 +1,4 @@
-
-$psakeFilePath = Resolve-Path "..\..\..\..\Build\psakefile.ps1"
+$current = get-location .
+Write-Host "Currect Location $current"
+$psakeFilePath = Resolve-Path ".\psakefile.ps1"
 & (Resolve-Path "..\Dependencies\psake*\tools\psake\psake.ps1") $psakeFilePath CreateNugetPackage
