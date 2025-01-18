571be664feb28565fef18483f29612f9e81b3940
Gooozmen <jguzman.softdev@gmail.com>
2025-01-18 15:30:05 -0300

    minor change

--- build/CreateAndPushPackage.ps1
+++ build/CreateAndPushPackage.ps1
@@ -1,5 +1,4 @@
 param ([string]$buildVersion = "0")
 $VersionInput ="2.0.0.$buildVersion"
 $psakeFilePath = Resolve-Path ".\psakefile.ps1"
-& (Resolve-Path "..\Dependencies\psake*\tools\psake\psake.ps1") 
--parameters @{"version"=$VersionInput} $psakeFilePath CreateAndPushPackage
+& (Resolve-Path "..\Dependencies\psake*\tools\psake\psake.ps1") -parameters @{"version"=$VersionInput} $psakeFilePath CreateAndPushPackage
