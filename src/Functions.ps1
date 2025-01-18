67134225a678775880ac85789feadd3bba854934
Gooozmen <jguzman.softdev@gmail.com>
2025-01-18 20:27:05 -0300

    minor change

--- src/Functions.ps1
+++ src/Functions.ps1
@@ -124,17 +124,28 @@ function Invoke-DotnetTests([string]$TestDllPath,
         Log-Info -Target $currentTarget -Message "Command: $command "
         
         & dotnet test $TestDllPath --logger 'trx;LogFileName=TestResults.trx' --results-directory $ResultsDirectory
+        Get-TestResults
 
         if ($LASTEXITCODE -ne 0) {
             Log-Error -Target $currentTarget -Message "Tests failed. Check the results in $ResultsDirectory"
             Exit 1
         }
-        Log-Success -Target $currentTarget -Message "Tests completed successfully. Results saved to $ResultsDirectory"
+        
         
     } catch {
         Log-Error -Target $currentTarget -Message "An error occurred while running xUnit tests: $_"
         Exit 1
     }
+}
+
+function Get-TestResults
+{
+    $currentTarget = "Get-TestResults"
+    Log-Info -Target $currentTarget -Message "Running File Verification for TestResults.trx"
+    if(Resolve-Path ("..\Artifacts\TestResults.trx")){
+        Log-Success -Target $currentTarget -Message "Test result is present at Artifacts folder"
+    }
+    Log-Error -Target $currentTarget -Message "File is not present at Destination Folder"
 }
 
 
